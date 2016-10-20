4c0d2d919a0d5da44ea59f1cc969dca8ebf25f14
melifluos <benjamin.chamberlain@gmail.com>
2016-10-20 09:06:41 +0100

    re-organised to share

--- embedding.py
+++ embedding.py
@@ -5,11 +5,9 @@ TODO: adapt this to work for bipartite graphs
 
 from utils import *
 from gensim.models import Word2Vec
+import node2vec
 
 graph = read_pickle('resources/X.p')
-
-# normalise the graph
-graph =
 
 
 def random_walk():
@@ -32,9 +30,25 @@ def learn_embeddings(walks):
     Learn embeddings by optimizing the Skipgram objective using SGD.
     '''
     walks = [map(str, walk) for walk in walks]
-    model = Word2Vec(walks, size=64, window=10, min_count=0, sg=1, workers=4,
+    model = Word2Vec(walks, size=32, window=10, min_count=0, sg=1, workers=4,
                      iter=1)
-    model.save_word2vec_format('../emb/test.emd')
+    model.save_word2vec_format('../emb/test32.emd')
+
+    return
+
+def main():
+    '''
+    Pipeline for representational learning for all nodes in a graph.
+    '''
+    nx_G = nx.read_edgelist('../../resources/test/test.edgelist', nodetype=int, create_using=nx.DiGraph())
+    for edge in nx_G.edges():
+        nx_G[edge[0]][edge[1]]['weight'] = 1
+    nx_G = nx_G.to_undirected()
+    G = node2vec.Graph(nx_G, False, 1, 1)
+    G.preprocess_transition_probs()
+    walks = G.simulate_walks(10, 80)
+    learn_embeddings(walks)
+
 
 
 if __name__ == '__main__':
