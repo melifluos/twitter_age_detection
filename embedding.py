c4827d930108adda662c0a6ece105b30110a8397
melifluos <ben@starcount.com>
2016-11-09 08:41:00 +0000

    generated public dataset embeddings

--- embedding.py
+++ embedding.py
@@ -28,31 +28,34 @@ def build_sentences():
     pass
 
 
-def learn_embeddings(walks, size):
+def learn_embeddings(walks, size, outpath):
     '''
     Learn embeddings by optimizing the Skipgram objective using SGD.
     '''
     walks = [map(str, walk) for walk in walks]
     model = Word2Vec(walks, size=size, window=10, min_count=0, sg=1, workers=4,
                      iter=1)
-    model.save_word2vec_format('resources/test/test' + str(size) + '.emd')
-
-    return
+    model.save_word2vec_format(outpath)
 
 
-def main(size):
+def main(size, paths):
     '''
     Pipeline for representational learning for all nodes in a graph.
     '''
-    nx_G = nx.read_edgelist('resources/test/test.edgelist', nodetype=int, create_using=nx.DiGraph())
+    nx_G = nx.read_edgelist(paths[0], nodetype=int, create_using=nx.DiGraph())
     for edge in nx_G.edges():
-        nx_G[edge[0]][edge[1]]['weight'] = 1
+       nx_G[edge[0]][edge[1]]['weight'] = 1
     nx_G = nx_G.to_undirected()
     G = node2vec.Graph(nx_G, False, 1, 1)
     G.preprocess_transition_probs()
     walks = G.simulate_walks(10, 80)
-    learn_embeddings(walks, size)
+    learn_embeddings(walks, size, paths[1])
 
 
 if __name__ == '__main__':
-    main(128)
+    inpaths = ['resources/test/blogcatalog/blogcatalog.edgelist', 'resources/test/flickr/flickr.edgelist',
+               'resources/test/youtube/youtube.edgelist']
+    outpaths = ['resources/test/blogcatalog/blogcatalog128.emd', 'resources/test/flickr/flickr128.emd',
+                'resources/test/youtube/youtube128.emd']
+    for paths in zip(inpaths, outpaths):
+        main(128, paths)
